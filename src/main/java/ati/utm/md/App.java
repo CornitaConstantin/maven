cf15204bf275e855f351dfd7e5365aa935ed1197
Constantin <costica.cornita@gmail.com>
2017-03-06 16:38:02 +0200

    joda-time

--- src/main/java/ati/utm/md/App.java
+++ src/main/java/ati/utm/md/App.java
@@ -1,13 +1,25 @@
 package ati.utm.md;
 
-/**
- * Hello world!
- *
- */
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+
 public class App 
 {
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Zile pina la noul an:" +
+        daysToNewYear(
+                LocalDate.now())
+                .getDays(
+        ));
+    }
+    public static Days daysToNewYear(LocalDate fromDate)
+    {
+        LocalDate newYear = fromDate
+                .plusYears(1)
+                .withDayOfYear(1);
+        return Days.daysBetween(fromDate, newYear);
+
     }
 }
